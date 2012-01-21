eea04ce98e2960e94ee9321d6bb0a34aab05669d
Colin Rice <dah4k0r@gmail.com>
2012-01-21 15:03:50 -0800

    New generation script

--- transfer_coins.py
+++ transfer_coins.py
@@ -35,5 +35,54 @@ for item in parse.sections():
     config.set(coin, 'method', coin_info['get_method'])
     if 'pattern' in coin_info:
         config.set(coin, 'key', coin_info['pattern'])
-    with open('./coins/%s' % item, 'wb') as configfile:
+    with open('./difficulty_sites/%s' % item, 'wb') as configfile:
+        config.write(configfile)
+        
+parse = ConfigParser.RawConfigParser()
+parse.read('../bitHopper/pools.cfg')
+parse.read('../bitHopper/user.cfg.default')
+
+for item in parse.sections():
+    pool_info = dict(parse.items(item))
+    if 'api_method' not in pool_info:
+        continue
+    config = ConfigParser.RawConfigParser()
+    config.add_section('general')
+    config.set('general', 'coin', pool_info['coin'])
+    config.set('general', 'name', item)
+    config.add_section('mine')
+    config.set('mine','address', pool_info['mine_address'])
+    if 'lp_address' in pool_info:
+        config.add_section('lp')
+        config.set('lp', 'address', pool_info['lp_address'])
+    config.add_section('api')
+    for k,v in pool_info.items():
+        if 'api' in k and 'duration' not in k:
+            k = k.replace('api_','')
+            config.set('api', k, v)
+            
+    config.add_section('duration')
+    for k,v in pool_info.items():
+        if 'api' in k and 'duration' in k:
+            k = k.replace('api_', '')
+            k = k.replace('_duration', '')
+            config.set('duration', k, v)
+            config.set('duration', 'method', pool_info['api_method'])
+            
+    config.add_section('ghash')
+    for k,v in pool_info.items():
+        if 'api' in k and 'ghashrate' in k:
+            k = k.replace('api_', '')
+            k = k.replace('_ghashrate', '')
+            config.set('ghash', k, v)
+    config.set('ghash', 'method', pool_info['api_method'])
+            
+            
+    config.add_section('shares')
+    if pool_info['api_method'] in ['json', 're', 'json_ec']:
+        config.set('shares', 'method', 'direct')
+    else:
+        config.set('shares', 'method', pool_info['api_method'].replace('re_',''))
+        
+    with open('./pools/%s' % item, 'wb') as configfile:
         config.write(configfile)
