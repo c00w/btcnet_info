b42100bd863ad5a5a1edcf2bb76398497e763926
Colin Rice <dah4k0r@gmail.com>
2012-01-21 12:27:59 -0800

    Initial work on dealing with exchanges

--- exchanges.py
+++ exchanges.py
@@ -6,10 +6,21 @@ except:
 import gevent, baseobject
 
 class Exchange(baseobject.Base_Object):
-    
+    def __init__(self, configfile, objects):
+        self.objects = objects
+        for coin in self.objects.coins_dict:
+            setattr(self, '_setup_%s' % coin, lambda section: self._setup_coin(section, coin))
+            
+        baseobject.Base_Object.__init__(self, configfile)
+            
+    def _setup_coin(self, section, coin):
+        setattr(self, coin + '_info', section)
+            
     def _setup_general(self, section):
         self.general_info = section
         self.name = section.get('name', 'Unknown')
         
     def _poll(self):
-        pass
+        """
+        Gets the appropriate values
+        """
