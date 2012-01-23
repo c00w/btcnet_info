6ece987f62366b83695ace18a4bcd061a14b9a59
Colin Rice <dah4k0r@gmail.com>
2012-01-23 17:00:59 -0500

    Better test cases

--- test.py
+++ test.py
@@ -5,18 +5,22 @@ import pools
 import gevent
 
 class TestStore(unittest.TestCase):
+    var = None
+    
+    def setUp(self):
+        if not self.var:
+            gevent.sleep(5)
+            self.var = True
+        
     def testCoins(self):
-        gevent.sleep(10)
         for item in __init__.get_coins():
             self.assertTrue(type(item) is coins.Coin )
             
     def testPools(self):
-        gevent.sleep(10)
         for item in __init__.get_pools():
             self.assertTrue(type(item) is pools.Pool )
             
     def testPolling(self):
-        gevent.sleep(10)
         count = len(__init__.get_pools())
         shares = 0
         for item in __init__.get_pools():
