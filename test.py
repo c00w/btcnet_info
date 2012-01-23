5e9906a9c78c68f80059e54e2fa14fca702b5514
Colin Rice <dah4k0r@gmail.com>
2012-01-23 14:50:58 -0500

    Added a proper test case

--- test.py
+++ test.py
@@ -6,14 +6,24 @@ import gevent
 
 class TestStore(unittest.TestCase):
     def testCoins(self):
-        gevent.sleep(30)
+        gevent.sleep(10)
         for item in __init__.get_coins():
             self.assertTrue(type(item) is coins.Coin )
             
     def testPools(self):
-        gevent.sleep(30)
+        gevent.sleep(10)
         for item in __init__.get_pools():
             self.assertTrue(type(item) is pools.Pool )
+            
+    def testPolling(self):
+        gevent.sleep(10)
+        count = len(__init__.get_pools())
+        shares = 0
+        for item in __init__.get_pools():
+            if 'shares' in dir(item):
+                shares += 1
+        print "%s/%s" % (shares, count)
+        
 
 if __name__ == "__main__":
     unittest.main()  
