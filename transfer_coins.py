caad620082ac9c39caf35a741d61ef34009ea682
Colin Rice <colin@daedrum.net>
2012-02-23 11:22:02 -0500

    Added duration_day_min_hour

--- transfer_coins.py
+++ transfer_coins.py
@@ -99,6 +99,31 @@ for item in parse.sections():
             config.set('duration', 'source', 'duration_hour')
             config.set('duration', 'source', 'duration_minute')
             
+        elif k == 'api_key_duration_day_hour_min':
+            config.add_section('duration_day')
+            config.set('duration_day', 'method', 're')
+            config.set('duration_day', 'source', pool_info['api_address']) 
+            config.set('duration_day', 'key', v)
+            config.set('duration_day', 'group', 1)
+        
+            config.add_section('duration_hour')
+            config.set('duration_hour', 'method', 're')
+            config.set('duration_hour', 'source', pool_info['api_address']) 
+            config.set('duration_hour', 'key', v)
+            config.set('duration_hour', 'group', 2)
+            
+            config.add_section('duration_minute')
+            config.set('duration_minute', 'method', 're')
+            config.set('duration_minute', 'source', pool_info['api_address']) 
+            config.set('duration_minute', 'key', v)
+            config.set('duration_minute', 'group', 3)
+            
+            config.add_section('duration')
+            config.set('duration', 'method', 'duration')
+            config.set('duration', 'method', 'duration_day')
+            config.set('duration', 'source', 'duration_hour')
+            config.set('duration', 'source', 'duration_minute')
+            
         elif 'duration' in k:
             print k
         elif 'api' in k and 'hashrate' in k:
