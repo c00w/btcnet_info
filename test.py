5fda3dfbcfeaed4e434717ce1ea8ad884ba6c95e
Colin Rice <colin@daedrum.net>
2012-11-09 14:23:08 -0500

    Update code, remove some whitespace, delete gpumax

--- test.py
+++ test.py
@@ -7,22 +7,22 @@ import json
 
 class TestStore(unittest.TestCase):
     var = None
-    
+
     @classmethod
     def setUpClass(self):
         __init__.get_coins()
         for i in range(120):
             gevent.sleep(1)
             print i
-        
+
     def testCoins(self):
         for item in __init__.get_coins():
             self.assertTrue(type(item) is coins.Coin )
-            
+
     def testPools(self):
         for item in __init__.get_pools():
             self.assertTrue(type(item) is pools.Pool )
-            
+
     def testMineAddress(self):
         valid = 0
         total = 0
@@ -31,7 +31,7 @@ class TestStore(unittest.TestCase):
             if item['mine.address'] != None:
                 valid += 1
         print 'Pools with addresses %s/%s' % (valid, total)
-            
+
     def testPolling(self):
         count = len(__init__.get_pools())
         shares = 0
@@ -44,14 +44,14 @@ class TestStore(unittest.TestCase):
                 pass
         print
         print "Valid Pools %s/%s" % (shares, count)
-        
+
         count = len(__init__.get_coins())
         shares = 0
         for item in __init__.get_coins():
             if item.exchange:
                 shares += 1
         print "Valid Coins w/ Exchanges %s/%s" % (shares, count)
-        
+
         count = len(__init__.get_coins())
         shares = 0
         for item in __init__.get_coins():
@@ -59,16 +59,16 @@ class TestStore(unittest.TestCase):
                 print "%s: %s" % (item.name, item.difficulty)
                 shares += 1
         print "Valid Coins w/ Difficulty %s/%s" % (shares, count)
-        
+
     def testApiDifficulty(self):
         for coin in __init__.get_coins():
             float(__init__.get_difficulty(coin.name))
-            
+
     def testApiExchange(self):
         for coin in __init__.get_coins():
             if coin.exchange:
                 float(__init__.get_exchange(coin.name))
-        
+
     def testBlockInfo(self):
         pool = __init__.get_pool('deepbit')
         print pool.blocks
@@ -83,4 +83,4 @@ class TestStore(unittest.TestCase):
     def testprintSlushShares(self):
         print __init__.get_pool('slush').shares
 if __name__ == "__main__":
-    unittest.main()  
+    unittest.main()
